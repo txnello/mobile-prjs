9a069bf2a52f49324ab90b279ce48650ac528db8
Manuel Tonello <49204166+txnello@users.noreply.github.com>
2023-05-27 15:04:45 +0200

    deleted useless projects

--- infinite_scroll/android/app/src/main/kotlin/com/example/infinite_scroll/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.infinite_scroll
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- qr_code_barcode/qrcode_barcode/android/app/src/main/kotlin/com/example/qrcode_barcode/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.qrcode_barcode
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
